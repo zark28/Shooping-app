b1237373ab8e0b3160e39a078c6341a3d3396640
zark28 <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>
2022-05-21 10:12:09 +0000

    first commit

---
+++ precache-manifest.e225f5d14bbedbe26879b88ef31156bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0f5281bc7e5c3de099e24d92850fb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6340ea58cbeea236de2",
+    "url": "/static/css/main.dad5a737.chunk.css"
+  },
+  {
+    "revision": "927d0b97267ec9c178d7",
+    "url": "/static/js/2.b7df5294.chunk.js"
+  },
+  {
+    "revision": "61c75ad1c6ad002fdd6f",
+    "url": "/static/js/3.a50d1155.chunk.js"
+  },
+  {
+    "revision": "a6340ea58cbeea236de2",
+    "url": "/static/js/main.879e941e.chunk.js"
+  },
+  {
+    "revision": "d326b2c53c39ac705f42",
+    "url": "/static/js/runtime~main.8e188af4.js"
+  }
+]);
